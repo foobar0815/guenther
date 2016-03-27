cc06381d322b9824cb430bf27ca65e6f3817133f
Marius Nuennerich <marius@nuenneri.ch>
2016-03-27 17:12:18 +0100

    Add some tests for Questionpool

---
+++ quizbot_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require "#{__dir__}/quizbot"
+
+class TestQuestionpool < Minitest::Test
+  def setup
+    @questionpool = Questionpool.new
+    # Load our actual questions as fixtures
+    @questionpool.load
+  end
+
+  def test_load
+    assert !@questionpool.instance_variable_get(:@questions).empty?
+  end
+
+  def test_any_key_has_value?
+    assert @questionpool.any_key_has_value?('dontcare', 'all')
+    assert @questionpool.any_key_has_value?('Category', 'Werkstoffe')
+    assert !@questionpool.any_key_has_value?('NonExistingCategory', 'dontcare')
+    assert !@questionpool.any_key_has_value?('Category', 'NonExisting')
+  end
+
+  def test_number_of_questions_per
+    qp = Questionpool.new
+    qp.instance_variable_set :@questions, [
+      {'Category' => 'category1'}, {'Category' => 'category1'},
+      { 'Category' => 'category2'}, {'Answer' => '42'},
+    ]
+    assert_equal "category1 (2), category2 (1)",
+                 qp.number_of_questions_per('Category')
+  end
+end
